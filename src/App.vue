673eddc27fca1c9e1f5f0142f249344176328065
Sebastian Weber <sebatian.weber80@gmail.com>
2024-03-17 15:56:49 +0100

    🔥 Remove previous example app to start over with a more simplistic use case to focus solely on Vitest.

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,6 @@
 <template>
-  <AppLayout>
-    <template #newsletter>
-      <MailForm @subscribe-newsletter="onSubscribeNewsletter" />
-    </template>
-    <template #headline>
-      <Headline font-color="red" text="todos.vue" class="headline" />
-    </template>
-    <template #default>
-      <Todos />
-    </template>
-  </AppLayout>
+  <h1>hello world</h1>
 </template>
 
-<script setup lang="ts">
-console.log("render <App />");
-
-onUpdated(() => {
-  console.log("updated <App />");
-});
-
-import { onUpdated } from "vue";
-import Headline from "./components/Headline.vue";
-import MailForm from "./components/MailForm.vue";
-import Todos from "./components/Todos.vue";
-import AppLayout from "./components/AppLayout.vue";
-
-const onSubscribeNewsletter = (firstName: string) => {
-  console.log(`Subscribe newsletter for ${firstName}`);
-};
-</script>
-<style lang="scss">
-.headline {
-  font-size: 100px;
-  font-weight: 100;
-  text-align: center;
-}
-</style>
+<script setup lang="ts"></script>
+<style lang="scss"></style>
--- src/components/types.ts
+++
@@ -1,8 +0,0 @@
-export interface TodoItemProps {
-  todo: {
-    id: number;
-    label: string;
-    date: string;
-    checked: boolean;
-  };
-}
