7a300130236027c332aa4f0f538d0ffe26aeb1ac
Sebastian Weber <sebastian.weber80@gmail.com>
2024-04-19 15:39:59 +0200

    ✅ Update axios mock cheat sheet test to use vi.importMock to mock axios only for individual test

--- vitest-cheat-sheets/axios.test.ts
+++ vitest-cheat-sheets/axios.test.ts
@@ -1,20 +1,15 @@
-/* eslint-disable no-unused-vars */
 import axios from "axios";
 import { expect, test, vi, describe } from "vitest";
 
-interface MockedAxios {
-  default: {
-    get: (url: string) => Promise<{ data: unknown }>;
-  };
-}
-
 describe("Axios mocking for individual test", () => {
   test("mocked axios", async () => {
-    const { default: ax } = await vi.importMock<MockedAxios>("axios");
+    const { default: ax } =
+      await vi.importMock<typeof import("axios")>("axios");
 
-    await ax.get("url");
+    const response = await ax.get("url");
 
-    expect(ax.get).toHaveBeenCalledWith("string");
+    expect(ax.get).toHaveBeenCalledWith("url");
+    expect(response.data.urlCharCount).toEqual(3);
   });
 
   test("actual axios is not mocked", async () => {
