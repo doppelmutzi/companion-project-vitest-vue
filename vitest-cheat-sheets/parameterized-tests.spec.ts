00588fefdc372dcdaeb39b533946cc8e5d894e86
Sebastian Weber <sebastian.weber80@gmail.com>
2024-04-20 17:52:31 +0200

    ✅ dynamic test descriptions with test.each

--- vitest-cheat-sheets/parameterized-tests.spec.ts
+++ vitest-cheat-sheets/parameterized-tests.spec.ts
@@ -2,7 +2,11 @@ import { test, expect } from "vitest";
 
 const inputs = ["Hello", "world", "!"];
 
-// https://vitest.dev/api/#test-each
 test.each(inputs)("Testing string length", (input) => {
+  expect(input.length).toBeGreaterThan(0);
+});
+
+// https://vitest.dev/api/#test-each
+test.each(inputs)("Testing string lengths of %s", (input) => {
   expect(input.length).toBeGreaterThan(0);
 });
